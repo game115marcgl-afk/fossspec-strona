1665bc363c1acc7e042bfdfb5d224369fa68d224
Foss Spec <fossspec@gmail.com>
2026-09-22 15:42:21 +0200

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 
+"use strict";
+require("dotenv").config();
+const app = require("./app");
+const connectDB = require("./config/db");
+
+const PORT = process.env.PORT || 3000;
+
+connectDB().then(() => {
+    app.listen(PORT, "0.0.0.0", () => {
+        console.log(`Foss Spec Forum działa na porcie ${PORT}`);
+    });
+});
