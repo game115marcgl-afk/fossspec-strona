9d249c15e31b55644cc385f5fc350dba8243ea67
Foss Spec <fossspec@gmail.com>
2026-09-26 14:08:14 +0200

    Refactor adminController for user listing
    Refactor adminController to export listUsers function directly and remove router usage.

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,13 +1,11 @@
 "use strict";
-const express = require("express");
-const router = express.Router();
-// Importujesz middleware, który sprawdza czy ktoś jest zalogowany i czy jest adminem
-const { requireAuth, requireAdmin } = require("../middleware/auth"); 
-const { listUsers } = require("../controllers/adminController");
+const User = require("../models/User");
+const asyncHandler = require("../utils/asyncHandler");
 
-// TYLKO admin może wykonać te operacje
-router.use(requireAuth, requireAdmin); 
+const listUsers = asyncHandler(async (req, res) => {
+    const users = await User.find().sort({ createdAt: -1 });
+    res.json(users);
+});
 
-router.get("/users", listUsers);
-
-module.exports = router;
+// Upewnij się, że to jest obiekt z funkcją!
+module.exports = { listUsers };
